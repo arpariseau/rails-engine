411e696f13923e31f66d1c6a40b239492be69a5e
Alex Pariseau <arpariseau@gmail.com>
2020-07-15 17:39:30 -0600

    Can now find the total revenue from all merchants between dates

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,21 @@ class Invoice < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
   has_many :transactions, dependent: :destroy
+
+  def self.total_revenue(start_date, end_date)
+    start = convert_dates(start_date)
+    finish = convert_dates(end_date)
+    query = "SELECT SUM(ii.unit_price * ii.quantity) AS revenue FROM invoices
+             JOIN invoice_items ii ON ii.invoice_id = invoices.id
+             JOIN transactions t ON t.invoice_id = invoices.id
+             WHERE t.result = 'success' AND invoices.created_at BETWEEN
+             '#{start.strftime("%Y-%m-%d")}' AND
+             '#{(finish + 1).strftime("%Y-%m-%d")}'"
+    ActiveRecord::Base.connection.execute(query)
+  end
+
+  def self.convert_dates(date_string)
+    date_array = date_string.split('-').map { |date| date.to_i }
+    Date.new(date_array[0], date_array[1], date_array[2])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get '/revenue', to: 'revenue#show'
       get '/merchants/most_revenue', to: 'merchant_revenues#index'
       get '/merchants/most_items', to: 'merchant_items#index'
       resources :items, except: [:new, :edit] do
